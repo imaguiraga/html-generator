070918b9b876f9ac4714f9186357c66a5a619184
Ibrahim <imaguiraga@hotmail.com>
2019-09-05 01:45:03 -0400

    icons

--- template/carbon/src/CarbonComponent.js
+++ template/carbon/src/CarbonComponent.js
@@ -1,5 +1,14 @@
 import React, {Component} from 'react';
-import { DataTable } from 'carbon-components-react';
+import { DataTable, Button } from 'carbon-components-react';
+
+import {Download24,Edit24,Settings24 } from '@carbon/icons-react';
+
+import 'carbon-components/css/carbon-components.css';
+import './CarbonComponent.css';
+
+import columns from './columnDefs'
+import data from './rowData'
+
 const {
   TableContainer,
   Table,
@@ -8,22 +17,32 @@ const {
   TableBody,
   TableCell,
   TableHeader,
+  TableToolbar,
+  TableToolbarSearch,
+  TableToolbarContent,
+  TableToolbarAction
 } = DataTable;
 
-import './CarbonComponent.css';
-
-import columns from './columnDefs'
-import data from './rowData'
 
 export default class CarbonComponent extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        columnDefs: columns,
-        rowData: data
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      columnDefs: columns,
+      rowData: data
     }
-  
+    this.onInputChange = this.onInputChange.bind(this);
+    this.action = this.action.bind(this);
+  }
+
+  onInputChange(e){
+
+  }
+
+  action(e){
+    console.log(e);
+  }
+
   render() {
     return (
       <div className="App">
@@ -35,6 +54,29 @@ export default class CarbonComponent extends Component {
           size={null}
           render={({ rows, headers, getHeaderProps }) => (
             <TableContainer title="DataTable">
+              <TableToolbar>
+                {/* pass in `onInputChange` change here to make filtering work */}
+                <TableToolbarSearch onChange={this.onInputChange} />
+                <TableToolbarContent>
+                  <TableToolbarAction
+                    icondescription="Download"
+                    onClick={(e)=> {this.action('TableToolbarAction - Download')}}
+                    ><Download24 /></TableToolbarAction>
+                  
+                  <TableToolbarAction
+                    icondescription="Edit"
+                    onClick={(e)=> {this.action('TableToolbarAction - Edit')}}
+                    ><Edit24 /></TableToolbarAction>
+            
+                  <TableToolbarAction
+                    icondescription="Settings"
+                    onClick={(e)=> {this.action('TableToolbarAction - Settings')}}
+                    ><Settings24 /></TableToolbarAction>
+                  <Button onClick={this.action('Add new row')} size="small" kind="primary">
+                    Add new
+                  </Button>
+                </TableToolbarContent>
+              </TableToolbar>
               <Table>
                 <TableHead>
                   <TableRow>
