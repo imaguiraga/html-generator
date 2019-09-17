b822017b22a4e5a02846712aa062ac97844536b7
Ibrahim <imaguiraga@hotmail.com>
2019-09-17 00:34:24 -0400

    update

--- template/gatsby-stie/src/components/carbon/CarbonComponent.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CarbonComponent from './CarbonComponent';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CarbonComponent />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
